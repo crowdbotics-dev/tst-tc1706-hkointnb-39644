667484c4b9b6e88fa46e6ef98642651b1860ecbf
Firstxutooxghka Lastopscqlwxcj <automation@crowdbotics.com>
2022-10-20 12:58:30 +0000

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native";
-
 export const globalOptions = {
   name: "tst_tc1706_hkointnb_39644",
   url: "https://tst_tc1706_hkointnb_39644.botics.co",
   api: "https://tst_tc1706_hkointnb_39644.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     "copy": "Routes available!"
   }
-}
+};
---
+++ screens/Untitled1/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled1 = () => {
+  return <View style={_styles.KwGLKFtA}>
+      </View>;
+};
+
+export default Untitled1;
+
+const _styles = StyleSheet.create({
+  KwGLKFtA: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
